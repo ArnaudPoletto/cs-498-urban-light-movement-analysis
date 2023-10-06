0e17722a5baf1b0dc5807e1643d3d1a1852b2a4b
Arnaud Poletto <arnaud.poletto@epfl.ch>
2023-10-06 23:35:01 +0200

    add sky ground segmentation implementation

---
+++ lumivid/sky_ground_segmentation/train.py
@@ -0,0 +1,227 @@
+# Ensure that the current working directory is this file
+import sys
+from pathlib import Path
+GLOBAL_DIR = Path(__file__).parent / '..' / '..'
+sys.path.append(str(GLOBAL_DIR))
+
+from ranger_adabelief import RangerAdaBelief # https://github.com/juntang-zhuang/Adabelief-Optimizer
+
+from lumivid.sky_ground_segmentation.dataset import get_dataloaders
+from lumivid.utils.model_utils import train
+
+import torch
+from torch import nn
+import torch.optim as optim
+from torch.optim import lr_scheduler
+import torchvision.models.segmentation as models
+
+TRAIN_SPLIT, TEST_SPLIT, VAL_SPLIT = 0.9, 0.095, 0.005
+EPOCHS = 4
+BATCH_SIZE = 4
+ACCUMULATION_STEPS = 8
+VALIDATION_STEPS = 400
+LEARNING_RATE = 0.001
+EPSILON = 1e-12
+BETAS = (0.95, 0.999)
+WEIGHT_DECAY = 0.01
+STEP_SIZE = 50
+GAMMA = 0.97
+
+CLASS_WEIGHTS = torch.tensor([[0.41311038, 0.58688962]])
+CLASS_WEIGHTS = CLASS_WEIGHTS.view(1, 2, 1, 1)
+
+DEVICE = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+def get_model(model_type: str, n_classes: int = 2, pretrained: bool = True) -> nn.Module:
+    """
+    Get model.
+
+    Args:
+        model_type (str): Model type.
+        n_classes (int, optional): Number of classes. Defaults to 2.
+        pretrained (bool, optional): Whether to use pretrained weights. Defaults to True.
+
+    Returns:
+        model (nn.Module): Model.
+    """
+    
+    assert model_type in ['deeplabv3resnet101', 'deeplabv3resnet50', 'deeplabv3mobilenetv3large'], f"❌ Invalid model type: {model_type}."
+    assert n_classes in [2, 21], f"❌ Invalid number of classes: {n_classes}."
+
+    # Get model type
+    if model_type == 'deeplabv3resnet101':
+        model = models.deeplabv3_resnet101
+    elif model_type == 'deeplabv3resnet50':
+        model = models.deeplabv3_resnet50
+    elif model_type == 'deeplabv3mobilenetv3large':
+        model = models.deeplabv3_mobilenet_v3_large
+
+    model = model(
+        weights='COCO_WITH_VOC_LABELS_V1' if pretrained else None,
+        weights_backbone='IMAGENET1K_V2' if pretrained else None,
+        progress=True, 
+        num_classes=21, 
+        aux_loss=True
+    ).to(DEVICE)
+
+    # Change the number of classes in the classifier to 2
+    in_features = model.classifier[4].in_channels
+    new_classifier = nn.Sequential(
+        nn.Conv2d(in_features, n_classes, kernel_size=1, stride=1)
+    ).to(DEVICE)
+    model.classifier[4] = new_classifier
+
+    n_params = sum(p.numel() for p in model.parameters() if p.requires_grad)
+    print(f"✅ Loaded {'pretrained ' if pretrained else ''}model {model_type} with {n_params:,} learnable parameters.")
+
+    return model
+
+def get_criterion(class_weights: torch.Tensor = CLASS_WEIGHTS) -> nn.Module:
+    """
+    Get criterion.
+
+    Args:
+        class_weights (torch.Tensor, optional): Class weights. Defaults to CLASS_WEIGHTS.
+    
+    Returns:
+        criterion (nn.Module): Criterion.
+    """
+
+    criterion = nn.BCEWithLogitsLoss(weight=class_weights.to(DEVICE))
+
+    print(f"✅ Loaded weighted binary cross entropy loss.")
+
+    return criterion
+
+def get_optimizer(
+        optimizer_type: str,
+        model: nn.Module,
+        learning_rate: float = LEARNING_RATE,
+        epsilon: float = EPSILON,
+        betas: tuple = BETAS,
+        weight_decay: float = WEIGHT_DECAY
+        ) -> optim.Optimizer:
+    """
+    Get optimizer.
+
+    Args:
+        optimizer_type (str): Optimizer type.
+        model (nn.Module): Model.
+        learning_rate (float, optional): Learning rate. Defaults to LEARNING_RATE.
+        epsilon (float, optional): Epsilon. Defaults to EPSILON.
+        betas (tuple, optional): Betas. Defaults to BETAS.
+        weight_decay (float, optional): Weight decay. Defaults to WEIGHT_DECAY.
+
+    Returns:
+        optimizer (optim.Optimizer): Optimizer.
+    """
+
+    assert optimizer_type in ['adam', 'adamw', 'ranger'], "❌ Invalid optimizer type."
+
+    if optimizer_type == 'adam':
+        optimizer = optim.Adam(model.parameters(), lr=learning_rate, eps=epsilon, betas=betas, weight_decay=weight_decay)
+    elif optimizer_type == 'adamw':
+        optimizer = optim.AdamW(model.parameters(), lr=learning_rate, eps=epsilon, betas=betas, weight_decay=weight_decay)
+    elif optimizer_type == 'ranger':
+        optimizer = RangerAdaBelief(model.parameters(), lr=learning_rate, eps=epsilon, betas=betas, weight_decay=weight_decay)
+
+    print(f"✅ Loaded {optimizer_type} optimizer with learning rate {learning_rate} and weight decay {weight_decay}.")
+
+    return optimizer
+
+def get_scheduler(optimizer: optim.Optimizer, step_size: int = STEP_SIZE, gamma: float = GAMMA) -> optim.lr_scheduler._LRScheduler:
+    """
+    Get scheduler.
+
+    Args:
+        optimizer (optim.Optimizer): Optimizer.
+
+    Returns:
+        scheduler (optim.lr_scheduler._LRScheduler): Scheduler.
+    """
+    
+    scheduler = lr_scheduler.StepLR(optimizer, step_size=step_size, gamma=gamma)
+    print(f"✅ Loaded scheduler with step size {step_size} and gamma {gamma}.")
+
+    return scheduler
+
+def ask_model():
+    """
+    Ask user for model type.
+    
+    Returns:
+        model_type (str): Model type.
+    """
+
+    int_to_model = {
+        '0': 'deeplabv3resnet101',
+        '1': 'deeplabv3resnet50',
+        '2': 'deeplabv3mobilenetv3large'
+    }
+
+    model_type = input("❓ Which model would you like to use?\n" + ''.join([f'\t{i}: {model}\n' for i, model in int_to_model.items()]))
+    assert model_type in ['0', '1', '2'], "❌ Invalid model type."
+
+    return int_to_model[model_type]
+
+def ask_pretrained():
+    """
+    Ask user for pretrained.
+    
+    Returns:
+        pretrained (bool): Whether to use pretrained weights.
+    """
+
+    pretrained = input("❓ Would you like to use pretrained weights? (y/n)\n")
+    assert pretrained in ['y', 'n'], "❌ Invalid pretrained."
+
+    return pretrained == 'y'
+
+
+def ask_optimizer():
+    """
+    Ask user for optimizer type.
+    
+    Returns:
+        optimizer_type (str): Optimizer type.
+    """
+
+    int_to_optimizer = {
+        '0': 'adam',
+        '1': 'adamw',
+        '2': 'ranger'
+    }
+
+    optimizer_type = input("❓ Which optimizer would you like to use?\n" + ''.join([f'\t{i}: {optimizer}\n' for i, optimizer in int_to_optimizer.items()]))
+
+    return int_to_optimizer[optimizer_type]
+
+if __name__ == '__main__':
+    model_type = ask_model()
+    pretrained = ask_pretrained()
+    optimizer_type = ask_optimizer()
+
+    train_loader, _, val_loader = get_dataloaders(TRAIN_SPLIT, TEST_SPLIT, BATCH_SIZE)
+
+    model = get_model(model_type, pretrained=pretrained)
+
+    criterion = get_criterion()
+    optimizer = get_optimizer(optimizer_type, model)
+    scheduler = get_scheduler(optimizer)
+
+    info_file_path = str(GLOBAL_DIR / 'data' / 'sky_ground_segmentation' / 'models' / f'{model_type}_{optimizer_type}_info.csv')
+    model_save_path = str(GLOBAL_DIR / 'data' / 'sky_ground_segmentation' / 'models' / f'{model_type}_{optimizer_type}.pth')
+    train(
+        model = model, 
+        train_loader = train_loader,
+        val_loader = val_loader,
+        criterion = criterion, 
+        optimizer = optimizer, 
+        scheduler = scheduler,
+        epochs = EPOCHS,
+        accumulation_steps = ACCUMULATION_STEPS,
+        validation_steps = VALIDATION_STEPS,
+        info_file_path = info_file_path,
+        model_save_path = model_save_path,
+        )
+
---
+++ lumivid/utils/model_utils.py
@@ -0,0 +1,246 @@
+from typing import List
+
+import os
+import numpy as np
+from tqdm import tqdm
+import matplotlib.pyplot as plt
+
+import torch
+import torch.nn as nn
+import torch.optim as optim
+from torch.utils.data import DataLoader
+from torch.cuda.amp import autocast, GradScaler
+
+def validate(
+        model: nn.Module, 
+        val_loader: DataLoader, 
+        criterion: nn.Module) -> float:
+    """
+    Validate model.
+
+    Args:
+        model (nn.Module): Model.
+        val_loader (DataLoader): Validation data loader.
+        criterion (nn.Module): Criterion.
+
+    Returns:
+        val_loss (float): Validation loss.
+    """
+
+    model.eval()
+
+    val_loss = 0
+    with torch.no_grad():
+        for data in val_loader:
+            input, mask = data
+
+            output = model(input)
+            output = output['out']
+            loss = criterion(output, mask)
+            val_loss += loss.item()
+            
+    model.train()
+
+    return val_loss / len(val_loader)
+
+def train(
+        model: nn.Module, 
+        train_loader: torch.utils.data.DataLoader,
+        val_loader: torch.utils.data.DataLoader,
+        criterion: nn.Module, 
+        optimizer: optim.Optimizer, 
+        scheduler: optim.lr_scheduler._LRScheduler,
+        epochs: int,
+        accumulation_steps: int,
+        validation_steps: int,
+        info_file_path: str,
+        model_save_path: str,
+        ) -> None:
+    """
+    Train model.
+
+    Args:
+        model (nn.Module): Model.
+        train_loader (torch.utils.data.DataLoader): Training data loader.
+        val_loader (torch.utils.data.DataLoader): Validation data loader.
+        criterion (nn.Module): Criterion.
+        optimizer (optim.Optimizer): Optimizer.
+        scheduler (optim.lr_scheduler._LRScheduler): Scheduler.
+        epochs (int): Number of epochs.
+        accumulation_steps (int): Number of steps to accumulate gradients.
+        validation_steps (int): Number of steps to validate.
+        info_file_path (str): Path to file to write training info to.
+        model_save_path (str): Path to file to save model to.
+    """
+
+    print(f"🚀 Training model for {epochs} epochs...")
+    
+    best_val_loss = float("inf")
+    train_loss = 0
+
+    # Refresh info file
+    if os.path.exists(info_file_path):
+        open(info_file_path, 'w').close()
+
+    with open(info_file_path, "a") as f:
+        f.write(f"epoch,step,validation_loss,train_loss\n")
+
+    scaler = GradScaler()
+
+    model.train()
+
+    for epoch in range(epochs):
+        bar = tqdm(total=len(train_loader))
+        for i, data in enumerate(train_loader):
+            inputs, masks = data
+
+            # Forward pass
+            with autocast():
+                try:
+                    outputs = model(inputs)
+                    outputs = outputs['out']
+                except Exception as e:
+                    print(f"❌ {e}")
+                    continue
+                loss = criterion(outputs, masks)
+
+            # Backward pass
+            scaler.scale(loss).backward()
+            train_loss += loss.item()
+
+            # Update weights every ACCUMULATION_STEPS
+            if (i+1) % accumulation_steps == 0:
+                # Update weights
+                scaler.step(optimizer)
+                scaler.update()
+                optimizer.zero_grad()
+
+                # Decay learning rate
+                scheduler.step()
+
+                # Show training loss
+                bar.set_description(f"➡️ Training loss: {train_loss / accumulation_steps:.5f}")
+                train_loss = 0
+
+            # Validate every VALIDATION_STEPS
+            if (i+1) % validation_steps == 0 or i == 0:
+                # Validate
+                val_loss = validate(model, val_loader, criterion)
+
+                # Print validation info to file
+                with open(info_file_path, "a") as f:
+                    f.write(f"{epoch},{i+1},{val_loss},{loss.item()}\n")
+
+                # Save model for best validation loss
+                if val_loss < best_val_loss:
+                    best_val_loss = val_loss
+                    torch.save(model.state_dict(), model_save_path)
+                    bar.clear()
+                    print(f"✅ Saved model with validation loss {val_loss:.5f}.")
+                    
+            bar.update(1)
+
+def get_class_weights(dataloader, n_classes=2, n_samples=1000):
+    """
+    Get class weights for a given dataloader.
+
+    Args:
+        dataloader: Dataloader to get class weights for.
+        n_samples: Number of samples to use for computing class weights.
+
+    Returns:
+        Class weights.
+    """
+
+    class_weights = np.zeros(n_classes)
+    total_samples = min(n_samples, len(dataloader))
+
+    bar = tqdm(total=total_samples)
+    for i, (_, masks) in enumerate(dataloader):
+        masks = masks.cpu().squeeze().numpy()[1]
+        for j in range(n_classes):
+            class_weights[j] += np.sum(masks == j)
+
+        bar.update(1)
+        
+        if i == total_samples - 1:
+            break
+
+    # Avoid division by zero
+    class_weights += 1e-10
+
+    class_weights = class_weights / np.sum(class_weights)
+    class_weights = 1 / class_weights
+    class_weights = class_weights / np.sum(class_weights)
+
+    return class_weights
+
+def show_learning_curves(
+    model_file_dir: str,
+    default_max_steps: int,
+    color_dict: dict = {},
+    window_size: int = 4,
+    ):
+    """
+    Show learning curves for models.
+
+    Args:
+        model_file_dir (str): Path to directory containing model files.
+        default_max_steps (int): Default maximum number of steps.
+        color_dict (dict): Dictionary mapping model names to colors.
+        window_size (int): Window size for smoothing.
+    """
+
+    def smooth_moving_avg(data, window_size: int) -> List[float]:
+        """
+        Smooth data using moving average.
+
+        Args:
+            data (list): Data to smooth.
+            window_size (int): Window size.
+
+        Returns:
+            moving_avgs (list): Smoothed data.
+        """
+        
+        cumsum = [0]
+        moving_avgs = []
+        for i, x in enumerate(data, 1):
+            cumsum.append(cumsum[i-1] + x)
+            if i >= window_size:
+                moving_avg = (cumsum[i] - cumsum[i-window_size]) / window_size
+                moving_avgs.append(moving_avg)
+            else:
+                moving_avgs.append(x)  # Keep original data points until window is full
+
+        return moving_avgs
+
+    model_file_paths = sorted([os.path.join(model_file_dir, filename) for filename in os.listdir(model_file_dir) if filename.endswith(".csv")])
+    validation_lossess = []
+    for model_file_path in model_file_paths:
+        with open(model_file_path, "r") as f:
+            name = model_file_path.split("/")[-1].split(".")[0]
+            name = name.replace('_info', '')
+
+            lines = f.readlines()[1:]
+            epochs = [int(line.split(",")[0]) for line in lines]
+            steps = [int(line.split(",")[1]) for line in lines]
+            max_steps = max(steps)
+            steps = [step * default_max_steps / max_steps for step in steps] # Normalize steps
+            idxs = [epoch * default_max_steps + step for epoch, step in zip(epochs, steps)]
+            validation_losses = [float(line.split(",")[2]) for line in lines]
+            smoothed_validation_losses = smooth_moving_avg(validation_losses, window_size)
+
+            validation_lossess.append((name, idxs, smoothed_validation_losses))
+
+    plt.figure(figsize=(10, 6))
+    for i, (name, idxs, validation_losses) in enumerate(validation_lossess):
+        color = color_dict[name] if name in color_dict else plt.get_cmap('tab20')(i)
+        plt.plot(idxs, validation_losses, label=name, color=color)
+    plt.yscale("log")
+    plt.title("Validation Losses")
+    plt.xlabel("Steps")
+    plt.ylabel("Cross-Entropy Loss")
+    plt.xlim(0, None)
+    plt.legend()
+    plt.show()
